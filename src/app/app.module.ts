26ba2dde6fd7c208140c0b05c67d352491953bd5
codeconcept <contact@codeconcept.fr>
2018-05-07 12:15:48 +0200

    template form for creating new bet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BetFormComponent } from './bet-form/bet-form.component';
+import { BetsComponent } from './bets/bets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BetFormComponent,
+    BetsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/bet-form/bet-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bet-form',
+  templateUrl: './bet-form.component.html',
+  styleUrls: ['./bet-form.component.css']
+})
+export class BetFormComponent implements OnInit {
+  bet = {title: '', description: ''};
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addBet(form) {
+    console.log(form.value);
+    form.reset();
+  }
+}
